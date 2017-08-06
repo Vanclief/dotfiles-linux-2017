9f08dcdabb6df72f902dc6f132e17c06a3b3882c
Franco Valencia <franco.avalencia@gmail.com>
2017-08-06 13:36:56 +0200

    Removed blank file

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -6,4 +6,4 @@
 #  fi
 #}
 #setopt promptsubst
-#PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+# PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
